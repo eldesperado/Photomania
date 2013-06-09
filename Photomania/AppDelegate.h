48298e1258d8691b11b730705d469a9be957a048
El Desperado <eldesperado@Els-MacBook-Pro.local>
2013-06-09 20:14:00 +0700

    Initial Commit

---
+++ Photomania/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photomania
+//
+//  Created by El Desperado on 6/9/13.
+//  Copyright (c) 2013 El Desperado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photomania/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Photomania
+//
+//  Created by El Desperado on 6/9/13.
+//  Copyright (c) 2013 El Desperado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
